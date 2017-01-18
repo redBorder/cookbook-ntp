fd7cde564e0b73c23b9f286b88175b9a4ca95a50
cjmateos <mateos83@gmail.com>
2017-01-18 12:34:06 +0100

    Disable package uninstallation in remove action

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -13,7 +13,7 @@ action :add do
       action :upgrade
       flush_cache [:before]
     end
-      
+
     template "/etc/ntp.conf" do
       source "ntp.conf.erb"
       owner "root"
@@ -23,7 +23,7 @@ action :add do
       variables(:server_0 => node["ntp"]["server_0"], :server_1 => node["ntp"]["server_1"], :server_2 => node["ntp"]["server_2"], :server_3 => node["ntp"]["server_3"])
       notifies :restart, "service[ntp]"
     end
-    
+
     service "ntp" do
       service_name "ntpd"
       ignore_failure true
@@ -39,7 +39,7 @@ end
 
 action :remove do
   begin
-    
+
     service "ntp" do
       service_name "ntpd"
       ignore_failure true
@@ -47,13 +47,12 @@ action :remove do
       action [:stop, :disable]
     end
 
-    file "/etc/ntp.conf" do
-      action :delete
-    end
+    #file "/etc/ntp.conf" do
+    #  action :delete
+    #end
 
     Chef::Log.info("Ntp cookbook has been processed")
   rescue => e
     Chef::Log.error(e.message)
   end
 end
-
